6306e0ec86e7df4f50be08a15edfe639f80cd7ee
Jaden Weiss <jadr2ddude@gmail.com>
2017-11-09 22:59:44 -0500

    Switch to demobox for demp

--- run.sh
+++ run.sh
@@ -9,4 +9,4 @@ trap cleanup EXIT
 echo "Welcome to the Panux demo."
 echo "You can install packages with lpkg install"
 echo "Don't know what to do? Try lpkg install sl && sl"
-docker run --name $NAME -it --rm -m 100m panux/panux:x86_64
+docker run --name $NAME -it --rm -m 100m panux/demobox
