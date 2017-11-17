9bee45c2cd602f44ff94f0a90ea2ac0ba405f785
Jaden Weiss <jadr2ddude@gmail.com>
2017-11-17 18:24:54 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,5 +8,6 @@ function cleanup() {
 trap cleanup EXIT
 echo "Welcome to the Panux demo."
 echo "You can install packages with lpkg install"
-echo "Don't know what to do? Try lpkg install sl && sl"
+echo "You can get root access by using su (no password set)"
+echo "Some basics are installed in this demo container: bash (entry shell), gcc, python (2 & 3), as well as all of the basic commands"
 docker run --name $NAME -it --rm -m 100m panux/demobox
